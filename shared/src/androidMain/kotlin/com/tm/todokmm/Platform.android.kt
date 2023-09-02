c478db29a40483aef743b01e9cd407e99146e3a4
Tushar Mahale <tmahale05@gmail.com>
2023-09-03 00:51:34 +0530

    todo app created :rocket:

---
+++ shared/src/androidMain/kotlin/com/tm/todokmm/Platform.android.kt
@@ -0,0 +1,7 @@
+package com.tm.todokmm
+
+class AndroidPlatform : Platform {
+    override val name: String = "Android ${android.os.Build.VERSION.SDK_INT}"
+}
+
+actual fun getPlatform(): Platform = AndroidPlatform()
